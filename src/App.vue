3829dbea88a077661b6c0c854122c561e8867ff6
George Litos <glitos@agroapps.gr>
2022-12-23 10:18:35 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import socksGreenImage from "./assets/images/socks_green.jpeg";
+
+const product = ref("Socks");
+const image = ref(socksGreenImage);
+const inStock = ref(true);
+
+
+</script>
+
+<template>
+  <div class="nav-bar"></div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img v-bind:src="image" />
+      </div>
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+      </div>
+    </div>
+  </div>
+</template>
